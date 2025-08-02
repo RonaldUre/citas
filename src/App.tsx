d4aa80749d11116c95db1d7ddada925cc8172c7d
ronald <roneduardo2014@gmail.com>
2025-08-02 09:13:42 -0400

    Primer commit: proyecto frontend base con Tailwind y PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">puta madre</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Gestion de citas</h1>
     </div>
   )
 }
